9ecc31172efdf6a33f31b6ae6922d06a808c8800
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2023-11-13 13:07:18 +0100

    update: add security check for input fields
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,6 +4,10 @@ import * as Yup from "yup";
 
 import supabase from "../lib/supabase";
 
+function cleanInput(value) {
+    return `${value}`.replace(/[<>%$]/gi, "");
+}
+
 function ContactForm() {
     const defaultContact = {
         name: "",
@@ -63,59 +67,78 @@ function ContactForm() {
                     validationSchema={ContactSchema}
                     onSubmit={handleSubmit}
                 >
-                    <Form>
-                        <Field name="name">
-                            {({ field, meta }) => (
-                                <div className="form-control">
-                                    <label mb="1.5" fontSize="sm" htmlFor="contact-name">
-                                        Name
-                                    </label>
-                                    <input {...field} placeholder="Enter name" id="contact-name" />
-                                    {meta.error && meta.touched && <p>{meta.error}</p>}
-                                </div>
-                            )}
-                        </Field>
-                        <Field name="email">
-                            {({ field, meta }) => (
-                                <div className="form-control">
-                                    <label mb="1.5" fontSize="sm" htmlFor="contact-email">
-                                        Email address{" "}
-                                        <span className="help-text">
-                                            (A test email notification will be sent to this email)
-                                        </span>
-                                    </label>
-                                    <input
-                                        {...field}
-                                        placeholder="Enter email"
-                                        id="contact-email"
-                                    />
+                    {({ setFieldValue }) => (
+                        <Form>
+                            <Field name="name">
+                                {({ field, meta }) => (
+                                    <div className="form-control">
+                                        <label mb="1.5" fontSize="sm" htmlFor="contact-name">
+                                            Name
+                                        </label>
+                                        <input
+                                            {...field}
+                                            placeholder="Enter name"
+                                            id="contact-name"
+                                            onChange={(e) => {
+                                                const value = cleanInput(e.target.value);
+                                                setFieldValue("name", value);
+                                            }}
+                                        />
+                                        {meta.error && meta.touched && <p>{meta.error}</p>}
+                                    </div>
+                                )}
+                            </Field>
+                            <Field name="email">
+                                {({ field, meta }) => (
+                                    <div className="form-control">
+                                        <label mb="1.5" fontSize="sm" htmlFor="contact-email">
+                                            Email address{" "}
+                                            <span className="help-text">
+                                                (A test email notification will be sent to this
+                                                email)
+                                            </span>
+                                        </label>
+                                        <input
+                                            {...field}
+                                            placeholder="Enter email"
+                                            id="contact-email"
+                                            onChange={(e) => {
+                                                const value = cleanInput(e.target.value);
+                                                setFieldValue("email", value);
+                                            }}
+                                        />
 
-                                    {meta.error && meta.touched && <p>{meta.error}</p>}
-                                </div>
-                            )}
-                        </Field>
-                        <Field name="body">
-                            {({ field, meta }) => (
-                                <div className="form-control">
-                                    <label mb="1.5" fontSize="sm" htmlFor="contact-body">
-                                        Body
-                                    </label>
-                                    <textarea
-                                        {...field}
-                                        id="contact-body"
-                                        placeholder="Describe what you are reaching out for."
-                                    />
-                                    {meta.error && meta.touched && <p>{meta.error}</p>}
-                                </div>
-                            )}
-                        </Field>
+                                        {meta.error && meta.touched && <p>{meta.error}</p>}
+                                    </div>
+                                )}
+                            </Field>
+                            <Field name="body">
+                                {({ field, meta }) => (
+                                    <div className="form-control">
+                                        <label mb="1.5" fontSize="sm" htmlFor="contact-body">
+                                            Body
+                                        </label>
+                                        <textarea
+                                            {...field}
+                                            id="contact-body"
+                                            placeholder="Describe what you are reaching out for."
+                                            onChange={(e) => {
+                                                const value = cleanInput(e.target.value);
+                                                setFieldValue("body", value);
+                                            }}
+                                        />
+                                        {meta.error && meta.touched && <p>{meta.error}</p>}
+                                    </div>
+                                )}
+                            </Field>
 
-                        <div className="button-container">
-                            <button type="submit" className="submit-button">
-                                {isSubmitting ? "Submitting..." : "Submit"}
-                            </button>
-                        </div>
-                    </Form>
+                            <div className="button-container">
+                                <button type="submit" className="submit-button">
+                                    {isSubmitting ? "Submitting..." : "Submit"}
+                                </button>
+                            </div>
+                        </Form>
+                    )}
                 </Formik>
             )}
         </section>
